7b80f2d4c3901ef0882d331ae2a19b1c6110b7e9
Vicky Staubly <vicky@steeds.com>
2013-12-15 18:22:25 -0500

    I think the SongWritable class is mostly complete... ready for initial testing.

--- src/SongWritable.java
+++ src/SongWritable.java
@@ -1,5 +1,112 @@
+import org.apache.hadoop.io.DataInput;
+import org.apache.hadoop.io.DataOutput;
 import org.apache.hadoop.io.WritableComparable;
 
 public class SongWritable extends SongInfo implements WritableComparable<SongWritable>
 {
+    public SongWritable()
+    {
+        super();
+    }
+
+    public int hashcode()
+    {
+        return artistName.hashcode() * 1637 + songTitle.hashcode();
+    }
+
+    private void writeString(DataOutput out, String s) throws IOException
+    {
+        if (s != null)
+            out.writeUTF(s);
+        else
+            out.writeUTF("");
+    }
+
+    @Override
+    public void write(DataOutput out) throws IOException
+    {
+        writeString(out,filename);
+        writeString(out,artistName);
+        writeString(out,artistID);
+        if (similarArtists != null) {
+            out.writeInt(similarArtists.length);
+            for (int i = 0; i < similarArtists.length; i++)
+                writeString(out,similarArtists[i]);
+        } else {
+            out.writeInt(0);
+        }
+        writeString(out,songTitle);
+        writeString(out,songID);
+        writeString(out,albumName);
+        out.writeInt(year);
+        out.writeDouble(loudness);
+        out.writeDouble(songHotness);
+        out.writeDouble(artistHotness);
+        out.writeDouble(tempo);
+    }
+
+    private String readString(DataInput in) throws IOException
+    {
+        return in.readUTF();
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException
+    {
+        filename = readString(in);
+        artistName = readString(in);
+        artistID = readString(in);
+        int len = in.readInt();
+        if (len > 0) {
+            similarArtists = new String[len];
+            for (int i = 0; i < similarArtists.length; i++)
+                similarArtists[i] = readString(in);
+        } else {
+            similarArtists = null;
+        }
+        readString(in,songTitle);
+        readString(in,songID);
+        readString(in,albumName);
+        year = in.readInt();
+        loudness = in.readDouble();
+        songHotness = in.readDouble();
+        artistHotness = in.readDouble();
+        tempo = in.readDouble();
+    }
+
+    public boolean compareStrings(String first, String second)
+    {
+        if ((first == null) && (second == null))
+            return true;
+        if (((first == null) && (second != null)) || ((first != null) && (second == null)))
+            return false;
+        return first.equals(second);
+    }
+
+    public boolean equals(SongWritable other)
+    {
+        if (other == null)
+            return false;
+        if (! compareStrings(songTitle,other.songTitle))
+            return false;
+        if (! compareStrings(artistName,other.artistName))
+            return false;
+        if (! compareStrings(songID,other.songID))
+            return false;
+        if (! compareStrings(albumName,other.albumName))
+            return false;
+        return true;
+    }
+
+    public int compareTo(SongWritable other)
+    {
+        int results = artistName.compareTo(other.artistName);
+        if (results != 0) return results;
+        results = songTitle.compareTo(other.songTitle);
+        if (results != 0) return results;
+        results = albumName.compareTo(other.albumName);
+        if (results != 0) return results;
+        results = songID.compareTo(other.songID);
+        return results;
+    }
 }

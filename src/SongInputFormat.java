44712c92a008cf470286b26eb45136e1acfea629
Vicky Staubly <vicky@steeds.com>
2013-12-17 01:16:23 -0500

    Initial versions of FileInputFormat and RecordReader for H5 format song files.

---
+++ src/SongInputFormat.java
@@ -0,0 +1,20 @@
+import java.io.Exception;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileSplit;
+import org.apache.hadoop.mapred.InputSplit;
+
+public class SongInputFormat extends FileInputFormat<Text,SongWritable>
+{
+    @Override
+    protected boolean isSplitable(FileSystem fs, Path filename)
+    {
+        return false;
+    }
+
+    @Override
+    public RecordReader<Text,SongWritable> getRecordReader(InputSplit split, JobConf jconf, Reporter reporter) throws IOException
+    {
+        return new SongReader((FileSplit)split, jconf);
+    }
+}
---
+++ src/SongReader.java
@@ -0,0 +1,35 @@
+import java.io.Exception;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileSplit;
+
+public class SongReader implements RecordReader<Text,SongWritable>
+{
+    private FileSplit fsplit;
+    private Configuration conf;
+    private boolean done = false;
+
+    public SongReader(FileSplit fsplit, Configuration conf) throws IOException
+    {
+        this.fsplit = fsplit;
+        this.conf = conf;
+    }
+
+    @Override
+    public Text createKey()
+    {
+        return new Text(fsplit.getPath().toString());
+    }
+
+    @Override
+    public SongWritable createValue()
+    {
+        done = true;
+        return new SongWritable(fsplit.getPath().toString());
+    }
+
+    @Override
+    public void close() throws IOException
+    {
+    }
+}

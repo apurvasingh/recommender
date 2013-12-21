c84e815fb5623cfdacacac055cedcd27d756e4e6
Vicky Staubly <vicky@steeds.com>
2013-12-21 17:47:28 -0500

    Added new .writeSong() method which actually works: 1. Saves each file in a format with each field on a line, in name-tab-value format 2. Creates directories as needed to maintain same directory tree structure

--- src/ConvertSongs.java
+++ src/ConvertSongs.java
@@ -1,7 +1,11 @@
+import java.io.IOException;
+import java.io.File;
+import java.io.PrintWriter;
+
 public class ConvertSongs extends RecurseSongs
 {
     private String destDir = null;
-    private String newExtension = ".xml";
+    private String newExtension = ".song";
 
     public ConvertSongs(String startDir, String destDir)
     {
@@ -22,8 +26,42 @@ public class ConvertSongs extends RecurseSongs
             String destName = destDir + base + newExtension;
             System.out.println("Converting " + srcName);
             System.out.println("    To " + destName);
+            writeSong(song,destName);
         } else {
             System.out.println("Unable to convert filename: " + srcName);
+        }
+    }
+
+    public void writeSong(SongInfo song, String destFilename)
+    {
+        try {
+            File songfile = new File(destFilename);
+            File parentDir = songfile.getParentFile();
+            if ((! parentDir.exists()) && (! parentDir.mkdirs())) {
+                System.err.println("Unable to create parent directory: " + parentDir);
+                return;
+            }
+
+            PrintWriter pw = new PrintWriter(destFilename);
+            pw.println("filename\t" + destFilename);
+            pw.println("original-filename\t" + song.getFilename());
+            pw.println("artist-name\t" + song.getArtistName());
+            pw.println("artist-id\t" + song.getArtistID());
+            pw.println("song-title\t" + song.getSongTitle());
+            pw.println("song-id\t" + song.getSongID());
+            pw.println("album-name\t" + song.getAlbumName());
+            pw.println("year\t" + song.getYear());
+            pw.println("loudness\t" + song.getLoudness());
+            pw.println("song-hotness\t" + song.getSongHotness());
+            pw.println("artist-hotness\t" + song.getArtistHotness());
+            pw.println("tempo\t" + song.getTempo());
+            String similarArtists[] = song.getSimilarArtists();
+            if (similarArtists != null)
+                for (String similarArtist : similarArtists)
+                    pw.println("similar-artist\t" + similarArtist);
+            pw.close();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
         }
     }
 

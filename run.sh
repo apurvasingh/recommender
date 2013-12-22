0a045bcae5f58ebfd1af2c1546e0310c39c82d50
Vicky Staubly <vicky@steeds.com>
2013-12-22 16:30:06 -0500

    Revised driver to match new Mapper output keys. Also, revised run script to always output to test_out     (and I can manually mv to unique name).

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-export HADOOP_CLASSPATH=$CLASSPATH
 # don't think I need "-Ddfs.client.read.shortcircuit=false"
 hadoop jar src/SongDoop.jar SongDoopDriver \
-     'MillionSongSubset/data/*/*/*' test_out9 \
+     'MillionSongSubset/data/*/*/*' test_out \
       MillionSongSubset/data/A/U/R/TRAURYF128F147805D.song \
       MillionSongSubset/data/A/I/U/TRAIUSY12903CF0C92.song
--- src/SongDoopDriver.java
+++ src/SongDoopDriver.java
@@ -36,7 +36,7 @@ public class SongDoopDriver extends Configured implements Tool
         job.setInputFormatClass(SongInputFormat.class);
         job.setMapperClass(SongDoopMapper.class);
         job.setReducerClass(SongDoopReducer.class);
-        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputKeyClass(IntWritable.class);
         job.setMapOutputValueClass(SongWritable.class);
         job.setOutputKeyClass(IntWritable.class);
         job.setOutputValueClass(Text.class);

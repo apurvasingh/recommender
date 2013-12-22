cf8ff5b5bf0a17ea03e34f8a59c3e459b26c8fc7
Vicky Staubly <vicky@steeds.com>
2013-12-22 16:10:39 -0500

    Modified run.sh script to use .song suffixes, and use next output dir.

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ export HADOOP_CLASSPATH=$CLASSPATH
 # don't think I need "-Ddfs.client.read.shortcircuit=false"
 hadoop jar src/SongDoop.jar SongDoopDriver \
       -libjars $CLASSPATH \
-     'MillionSongSubset/data/*/*/*' test_out8 \
-      MillionSongSubset/data/A/U/R/TRAURYF128F147805D.h5 \
-      MillionSongSubset/data/A/I/U/TRAIUSY12903CF0C92.h5
+     'MillionSongSubset/data/*/*/*' test_out9 \
+      MillionSongSubset/data/A/U/R/TRAURYF128F147805D.song \
+      MillionSongSubset/data/A/I/U/TRAIUSY12903CF0C92.song

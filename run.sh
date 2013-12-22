66c24b09c70fc84990466d09e6688873fb1db34c
Vicky Staubly <vicky@steeds.com>
2013-12-22 16:20:52 -0500

    Hadoop job no longer depends on external JARs, so remove -libjars option.

--- run.sh
+++ run.sh
@@ -3,7 +3,6 @@
 export HADOOP_CLASSPATH=$CLASSPATH
 # don't think I need "-Ddfs.client.read.shortcircuit=false"
 hadoop jar src/SongDoop.jar SongDoopDriver \
-      -libjars $CLASSPATH \
      'MillionSongSubset/data/*/*/*' test_out9 \
       MillionSongSubset/data/A/U/R/TRAURYF128F147805D.song \
       MillionSongSubset/data/A/I/U/TRAIUSY12903CF0C92.song
